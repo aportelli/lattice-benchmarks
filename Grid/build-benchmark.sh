4d881dcd5443248d841c2397e214097a43c362a3
Antonin Portelli <antonin.portelli@me.com>
2023-01-24 20:08:40 +0000

    Grid benchmark build system fix

---
+++ Grid/build-benchmark.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC1090,SC1091
+
+set -euo pipefail
+
+if (( $# != 2 )); then
+    echo "usage: $(basename "$0") <environment directory> <config>" 1>&2
+    exit 1
+fi
+env_dir=$1
+cfg=$2
+
+call_dir=$(pwd -P)
+script_dir="$(dirname "$(readlink -f "${BASH_SOURCE:-$0}")")"
+cd "${env_dir}"
+env_dir=$(pwd -P)
+cd "${call_dir}"
+build_dir="${env_dir}/build/Grid-benchmarks/${cfg}"
+if [ -d "${build_dir}" ]; then
+    echo "error: directory '${build_dir}' exists"
+    exit 1
+fi
+mkdir -p "${build_dir}"
+source "${env_dir}/env.sh"
+entry=$(jq ".configs[]|select(.name==\"${cfg}\")" "${env_dir}"/grid-config.json)
+env_script=$(echo "${entry}" | jq -r ".\"env-script\"")
+cd "${build_dir}" || return
+source "${env_dir}/${env_script}"
+"${script_dir}/configure" --with-grid="${env_dir}/prefix/grid_${cfg}" \
+                          --prefix="${env_dir}/prefix/gridbench_${cfg}"
+make -j 128
+make install
+cd "${call_dir}"
