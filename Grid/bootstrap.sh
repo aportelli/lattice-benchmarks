c9d3d0bbc500c43a01b8c20e229199c7e7555d74
Antonin Portelli <antonin.portelli@me.com>
2023-01-27 12:47:05 +0000

    JSON URL fix

--- Grid/bootstrap.sh
+++ Grid/bootstrap.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-json_url='https://github.com/nlohmann/json/blob/bc889afb4c5bf1c0d8ee29ef35eaaf4c8bef8a5d/single_include/nlohmann/json.hpp'
+json_url='https://raw.githubusercontent.com/nlohmann/json/bc889afb4c5bf1c0d8ee29ef35eaaf4c8bef8a5d/single_include/nlohmann/json.hpp'
 
 if [ ! -f json.hpp ]; then
   wget ${json_url}
