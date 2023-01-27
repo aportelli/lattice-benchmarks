928528ccfe5d0b4e17aced0a641ab050260dcc18
Antonin Portelli <antonin.portelli@me.com>
2023-01-27 12:34:49 +0000

    remove json.hpp from repo

--- Grid/bootstrap.sh
+++ Grid/bootstrap.sh
@@ -2,5 +2,10 @@
 
 set -euo pipefail
 
+json_url='https://github.com/nlohmann/json/blob/bc889afb4c5bf1c0d8ee29ef35eaaf4c8bef8a5d/single_include/nlohmann/json.hpp'
+
+if [ ! -f json.hpp ]; then
+  wget ${json_url}
+fi
 mkdir -p .buildutils/m4
 autoreconf -fvi
